c5147d5d5d7c8c92a2dedc28d73472576731c99f
superpozycja <anna@superpozycja.net>
2024-08-06 22:55:36 +0200

    test file operations

--- main.go
+++ main.go
@@ -1,9 +1,40 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"strings"
 )
 
+var codebaseDir = flag.String("d", "", "directory of the codebase to crawl")
+
 func main() {
-	fmt.Println("oho")
+	flag.Parse()
+	if *codebaseDir == "" {
+		log.Fatal("no dir provided")
+	}
+	files, err := ioutil.ReadDir(*codebaseDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, file := range files {
+		if !file.IsDir() {
+			fmt.Println(file.Name())
+			fmt.Println("")
+			b, err := ioutil.ReadFile(*codebaseDir + file.Name())
+			if err != nil {
+				log.Fatal(err)
+			}
+			data := strings.Split(string(b), "\n")
+			for _, line := range data {
+				if strings.Contains(line, "import") {
+					fmt.Println(line)
+				}
+			}
+			fmt.Println("")
+			fmt.Println("")
+		}
+	}
 }
